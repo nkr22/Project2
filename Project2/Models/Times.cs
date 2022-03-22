abc73b12abf1aaadbdd191bb871e2114ecf6314e
ashleydenison <dashley9@byu.edu>
2022-03-21 18:29:00 -0600

    Models

---
+++ Project2/Models/Times.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2.Models
+{
+    public class Times
+    {
+        [Key]
+        [Required]
+        public int SlotId {get; set;}
+
+        [Required]
+        public string Month { get; set; }
+
+        [Required]
+        public int Day { get; set; }
+
+        [Required]
+        public string Time { get; set; }
+    }
+}
---
+++ Project2/Models/Tour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2.Models
+{
+    public class Tour
+    {
+        [Key]
+        [Required]
+        public int TourId { get; set; }
+
+        [Required]
+        // Build Foreign Key Relationship
+        public int SlotId { get; set; }
+        //BUILD MODEL FIRST
+        public Times Slot { get; set; }
+
+        [Required]
+        public int GroupId { get; set; }
+        public ContactInfo Group { get; set; }
+    }
+}
